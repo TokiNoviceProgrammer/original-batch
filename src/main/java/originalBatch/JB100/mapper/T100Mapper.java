967fc6d10850b3f272b04eed5a41e5e23c050f36
tokizaki <tokizaki@tdc.co.jp>
2024-08-16 13:40:26 +0900

    [test]テストコミット03

--- src/main/java/originalBatch/JB100/mapper/T100Mapper.java
+++ src/main/java/originalBatch/JB100/mapper/T100Mapper.java
@@ -9,6 +9,10 @@ import originalBatch.JB100.model.T100;
  * T100へのDB操作
  */
 public interface T100Mapper {
+	/**
+	 * T100のidの最大値を取得
+	 * @return
+	 */
 	public Integer selectMaxId();
 
 	public void insertT100(T100 t100);
