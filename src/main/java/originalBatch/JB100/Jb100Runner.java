13ba1240d70f505f4dd7c8dab2145919f13267dc
tokizaki <tokizaki@tdc.co.jp>
2024-08-16 16:31:19 +0900

    [test]テストコミット07

--- src/main/java/originalBatch/JB100/Jb100Runner.java
+++ src/main/java/originalBatch/JB100/Jb100Runner.java
@@ -19,6 +19,10 @@ public class Jb100Runner implements CommandLineRunner, ExitCodeExceptionMapper {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Jb100Runner.class);
 
+	/**
+	 * コンストラクタ
+	 * @param jb100Service
+	 */
 	public Jb100Runner(Jb100Service jb100Service) {
 		this.jb100Service = jb100Service;
 	}
